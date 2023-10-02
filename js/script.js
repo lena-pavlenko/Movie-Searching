83ad946545e915ec154c708f3d0f227872e81700
Lena Pavlenko <pawlencko.alena2010@yandex.ru>
2023-10-02 14:48:19 +0300

    refactored pagination script

--- js/script.js
+++ js/script.js
@@ -32,7 +32,7 @@ async function getFilm() {
 
     } catch (error) {
         console.log(error);
-        throw error // или return Promise.reject()
+        throw error 
     }
    
 }
@@ -188,6 +188,7 @@ function removeAlarmMessage() {
 
 function paginationHandler(e) {
     e.preventDefault()
+    
     if (e.target.closest('.js-page-item')) {
         document.querySelectorAll('.js-page-item').forEach((pageItem) => {
             pageItem.classList.remove('active')
@@ -251,93 +252,85 @@ function paginationHandler(e) {
     }
 }
 
+function renderPrevArrow(isDisabled = true) {
+    return `
+        <li class="page-item page-item_prev ${isDisabled ? 'disabled' : ''}">
+            <a class="page-link" href="#" aria-label="Previous">
+                <span aria-hidden="true">&laquo;</span>
+            </a>
+        </li>
+    `
+}
+
+function renderNextArrow(isDisabled = false) {
+    return `
+        <li class="page-item page-item_next ${isDisabled ? 'disabled' : ''}">
+            <a class="page-link" href="#" aria-label="Next">
+                <span aria-hidden="true">&raquo;</span>
+            </a>
+        </li>
+    `
+}
+
+function renderLastPage(pagesCount) {
+    return `
+        <span class="gap-item">...</span>
+        <li class="page-item js-page-item"><a class="page-link" href="#">${pagesCount}</a></li>
+    `
+}
+
+function renderButtons(index, stopValue, current) {
+    let buttonsHtml = ''
+    for (let i = index; i <= stopValue; i++) {
+        buttonsHtml += `<li class="page-item js-page-item ${current === i ? 'active' : ''}"><a class="page-link" href="#">${i}</a></li>`
+    }
+    return buttonsHtml
+}
+
+function renderGap() {
+    return `<span class="gap-item">...</span>`
+}
+
+function renderFirstPage() {
+    return `<li class="page-item js-page-item"><a class="page-link" href="#">1</a></li>`
+}
+
 function renderPages(pages, current) {
     paginationContainer.innerHTML = '';
+    const count = 3
 
-    if (current <= 3) {
-        paginationContainer.insertAdjacentHTML('beforeend', `
-            <li class="page-item js-page-item ${current === 1 ? 'active' : ''}"><a class="page-link" href="#">1</a></li>
-            <li class="page-item js-page-item ${current === 2 ? 'active' : ''}"><a class="page-link" href="#">2</a></li>
-            <li class="page-item js-page-item ${current === 3 ? 'active' : ''}"><a class="page-link" href="#">3</a></li>
-        `)
-
-        paginationContainer.insertAdjacentHTML('afterbegin', `
-            <li class="page-item page-item_prev disabled">
-                <a class="page-link" href="#" aria-label="Previous">
-                    <span aria-hidden="true">&laquo;</span>
-                </a>
-            </li>
-        `
-        )
-
-        paginationContainer.insertAdjacentHTML('beforeend', `
-            <span class="gap-item">...</span>
-            <li class="page-item js-page-item"><a class="page-link" href="#">${pages}</a></li>
-            <li class="page-item page-item_next">
-                <a class="page-link" href="#" aria-label="Next">
-                    <span aria-hidden="true">&raquo;</span>
-                </a>
-            </li>
-        `
-        )
+    if (pages === 1) {
+        return
     }
 
-    if (current > 3 && current < pages-3) {
-        paginationContainer.insertAdjacentHTML('beforeend', `
-            <li class="page-item js-page-item"><a class="page-link" href="#">1</a></li>
-            <span class="gap-item">...</span>
-            <li class="page-item js-page-item"><a class="page-link" href="#">${current-1}</a></li>
-            <li class="page-item js-page-item active"><a class="page-link" href="#">${current}</a></li>
-            <li class="page-item js-page-item"><a class="page-link" href="#">${current+1}</a></li>
-        `)
+    if (pages <= (count + 1) * 2) {
+        paginationContainer.insertAdjacentHTML('beforeend', renderButtons(1, pages, current))
+        
+    } else {
 
-        paginationContainer.insertAdjacentHTML('afterbegin', `
-            <li class="page-item page-item_prev">
-                <a class="page-link" href="#" aria-label="Previous">
-                    <span aria-hidden="true">&laquo;</span>
-                </a>
-            </li>
-        `
-        )
-
-        paginationContainer.insertAdjacentHTML('beforeend', `
-            <span class="gap-item">...</span>
-            <li class="page-item js-page-item"><a class="page-link" href="#">${pages}</a></li>
-            <li class="page-item page-item_next">
-                <a class="page-link" href="#" aria-label="Next">
-                    <span aria-hidden="true">&raquo;</span>
-                </a>
-            </li>
-        `
-        )
-    }
-
-    if (current >= pages-3) {
-        paginationContainer.insertAdjacentHTML('beforeend', `
-            <li class="page-item js-page-item"><a class="page-link" href="#">1</a></li>
-        `)
-
-        paginationContainer.insertAdjacentHTML('afterbegin', `
-            <li class="page-item page-item_prev">
-                <a class="page-link" href="#" aria-label="Previous">
-                    <span aria-hidden="true">&laquo;</span>
-                </a>
-            </li>
-        `
-        )
-
-        paginationContainer.insertAdjacentHTML('beforeend', `
-            <span class="gap-item">...</span>
-            <li class="page-item js-page-item ${current === pages-2 ? 'active' : ''}"><a class="page-link" href="#">${pages-2}</a></li>
-            <li class="page-item js-page-item ${current === pages-1 ? 'active' : ''}"><a class="page-link" href="#">${pages-1}</a></li>
-            <li class="page-item js-page-item ${current === pages ? 'active' : ''}"><a class="page-link" href="#">${pages}</a></li>
-            <li class="page-item page-item_next disabled">
-                <a class="page-link" href="#" aria-label="Next">
-                    <span aria-hidden="true">&raquo;</span>
-                </a>
-            </li>
-        `
-        )
+        if (current <= count) {
+            paginationContainer.insertAdjacentHTML('beforeend', renderButtons(1, count, current))
+            paginationContainer.insertAdjacentHTML('afterbegin', renderPrevArrow())
+            paginationContainer.insertAdjacentHTML('beforeend', renderLastPage(pages))
+            paginationContainer.insertAdjacentHTML('beforeend', renderNextArrow())
+        }
+    
+        if (current > count && current <= pages - count) {
+            paginationContainer.insertAdjacentHTML('beforeend', renderFirstPage())
+            paginationContainer.insertAdjacentHTML('beforeend', renderGap())
+            paginationContainer.insertAdjacentHTML('beforeend', renderButtons(current - Math.floor(count / 2), current + Math.floor(count / 2), current))
+            paginationContainer.insertAdjacentHTML('afterbegin', renderPrevArrow(false))
+            paginationContainer.insertAdjacentHTML('beforeend', renderLastPage(pages))
+            paginationContainer.insertAdjacentHTML('beforeend', renderNextArrow())
+        }
+    
+        if (current > pages - count) {
+            paginationContainer.insertAdjacentHTML('beforeend', renderFirstPage())
+            paginationContainer.insertAdjacentHTML('afterbegin', renderPrevArrow(false))
+            paginationContainer.insertAdjacentHTML('beforeend', renderGap())
+            paginationContainer.insertAdjacentHTML('beforeend', renderButtons(pages - count + 1, pages, current))
+            paginationContainer.insertAdjacentHTML('beforeend', renderNextArrow(true))
+        }
     }
 }
 
